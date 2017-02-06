de379467d8edb936f95404a5729105907bbade25
CAndRyan <CAndRyan@users.noreply.github.com>
2017-02-06 07:13:30 -0500

    Updated standup functions

--- Modules/Standup.psm1
+++ Modules/Standup.psm1
@@ -1,13 +1,158 @@
+function Import-StandupXml {
+	param(
+		[parameter()]
+		[String]
+		$Path = "$env:USERPROFILE\Documents\standup.xml"
+	)
+	
+	$dat = New-Object -TypeName PSObject
+	$dat |Add-Member -MemberType NoteProperty -Name NextId -Value 0 -PassThru |
+		Add-Member -MemberType NoteProperty -Name Days -Value (New-Object System.Collections.Generic.List[PSObject])
+	
+	if (Test-Path -Path $Path) {
+		$xml = Import-Clixml -Path $Path
+		foreach ($obj in $xml.Days) {
+			$dat.Days.Add($obj)
+			
+			foreach ($item in $obj.Notes) {
+				$count++
+			}
+		}
+		
+		$dat.NextId = $xml.NextId
+	}
+	
+	return $dat
+}
+
 function Add-StandupNote {
 	param(
 		[parameter()]
 		[String]
 		$Note,
 		
+		[parameter()]
+		[String]
+		$Path = "$env:USERPROFILE\Documents\standup.xml",
+		
 		[parameter()]	#Default to the current date
 		[String]
-		$Date
+		$DateString = ""
 	)
 	
+	$date = $null
+	if ($DateString -ne "") {
+		$date = ([DateTime]$DateString).ToString("d")
+	}
+	else {
+		$date = (Get-Date).ToString("d")
+	}
 	
+	$dat = Import-StandupXml -Path $Path
+	
+	$curr = New-Object -TypeName PSObject
+	$curr |Add-Member -MemberType NoteProperty -Name Date -Value $date -PassThru |
+		Add-Member -MemberType NoteProperty -Name Notes -Value (New-Object System.Collections.Generic.List[PSObject])
+	
+	$selected = $dat.Days |Where-Object {$_.Date -eq $date}
+	if ($selected -ne $null) {
+		foreach ($obj in $selected.Notes) {
+			$curr.Notes.Add($obj)
+		}
+		
+		$dat.Days.Remove($selected) |Out-Null
+	}
+	
+	$new = New-Object -TypeName PSObject
+	$new |Add-Member -MemberType NoteProperty -Name Id -Value $count -PassThru |
+		Add-Member -MemberType NoteProperty -Name Note -Value $Note
+	$curr.Notes.Add($new)
+	$dat.Days.Add($curr)
+	$dat.NextId = $dat.NextId + 1
+	$dat.Days = $dat.Days |Sort-Object -Property Date
+	
+	$dat |Export-Clixml -Path $Path
+}
+
+function Get-StandupNotes {
+	param(
+		[parameter()]	#Default to the current date
+		[String]
+		$DateString = "",
+		
+		[parameter()]
+		[String]
+		$Path = "$env:USERPROFILE\Documents\standup.xml"
+	)
+	
+	$date = $null
+	if ($DateString -ne "") {
+		$date = ([DateTime]$DateString).ToString("d")
+	}
+	else {
+		$date = (Get-Date).ToString("d")
+	}
+	
+	$dat = Import-StandupXml -Path $Path
+	
+	$curr = New-Object -TypeName PSObject
+	$curr |Add-Member -MemberType NoteProperty -Name Date -Value $date -PassThru |
+		Add-Member -MemberType NoteProperty -Name Notes -Value (New-Object System.Collections.Generic.List[PSObject])
+	
+	$selected = $dat.Days |Where-Object {$_.Date -eq $date}
+	if ($selected -ne $null) {
+		foreach ($obj in $selected.Notes) {
+			$curr.Notes.Add($obj)
+		}
+	}
+	
+	return $curr.Notes
+}
+
+function Remove-StandupNote {
+	param(
+		[parameter(Mandatory=$true)]
+		[Int]
+		$Id,
+	
+		[parameter()]	#Default to the current date
+		[String]
+		$DateString = "",
+		
+		[parameter()]
+		[String]
+		$Path = "$env:USERPROFILE\Documents\standup.xml"
+	)
+	
+	$date = $null
+	if ($DateString -ne "") {
+		$date = ([DateTime]$DateString).ToString("d")
+	}
+	else {
+		$date = (Get-Date).ToString("d")
+	}
+	
+	$dat = Import-StandupXml -Path $Path
+	
+	$curr = New-Object -TypeName PSObject
+	$curr |Add-Member -MemberType NoteProperty -Name Date -Value $date -PassThru |
+		Add-Member -MemberType NoteProperty -Name Notes -Value (New-Object System.Collections.Generic.List[PSObject])
+	
+	$selected = $dat |Where-Object {$_.Date -eq $date}
+	if ($selected -ne $null) {
+		foreach ($obj in $selected.Notes) {
+			$curr.Notes.Add($obj)
+		}
+		
+		$dat.Days.Remove($selected) |Out-Null
+	}
+	
+	$rem = $curr.Notes |Where-Object {$_.Id -eq $Id}
+	if ($rem -ne $null) {
+		$curr.Notes.Remove($rem) |Out-Null
+		
+		$dat.Days.Add($curr)
+		$dat.Days = $dat.Days |Sort-Object -Property Date
+		$dat.Days |Export-Clixml -Path $Path
+	}
 }
