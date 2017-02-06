8adb7d285f26a3ea7983f0b1484fbf55cf583e73
CAndRyan <CAndRyan@users.noreply.github.com>
2017-02-06 16:17:51 -0500

    Updated Git hooks and added bare repositories to Get-GitRepos

--- Hooks/post-commit.ps1
+++ Hooks/post-commit.ps1
@@ -1,3 +1,3 @@
 $dir = ("c:" + ("$($args[0])".Substring(2) -replace "/", "\"))
 
-New-Item -Path "$dir\TEST.txt"
+New-Item -Path "$dir\..\..\TEST.txt" |Out-Null
--- Modules/Git.psm1
+++ Modules/Git.psm1
@@ -1,13 +1,35 @@
 function Get-GitRepos {
+	[CmdletBinding()]
 	param(
 		[parameter()]
 		[String]
-		$Directory = "C:\"
+		$Directory = "C:\",
+		
+		[parameter()]
+		[Switch]
+		$IncludeBare
 	)
 	
-	$verboseOn = ($PSBoundParameters['Verbose'] -eq $true)
+	$sw = $null
+	$verbosity = $VerbosePreference
+	$sw = [System.Diagnostics.Stopwatch]::StartNew()
+	if ($Verbose) {
+		$VerbosePreference = "continue"
+	}
 	
-	Get-ChildItem -Path $Directory -Recurse -Force -ErrorAction silentlycontinue -Filter ".git" |
-		Select-Object -ExpandProperty FullName |
-		Foreach-Object  {$_ -replace "\\.git$", ""}
+	if ($IncludeBare) {
+		Get-ChildItem -Path $Directory -Recurse -Force -ErrorAction silentlycontinue -File -Filter "HEAD" |
+			Select-Object -ExpandProperty FullName |
+			Foreach-Object  {$_ -replace "(\\\.git)?\\HEAD$", ""} |
+			Where-Object {-not ($_ -match ".*\\\.git\\.*")}
+	}
+	else {
+		Get-ChildItem -Path $Directory -Recurse -Force -ErrorAction silentlycontinue -Directory -Filter ".git" |
+			Select-Object -ExpandProperty FullName |
+			Foreach-Object  {$_ -replace "\\\.git$", ""}
+	}
+	
+	$sw.Stop()
+	Write-Verbose "Execution took $($sw.Elapsed.TotalSeconds) seconds"
+	$VerbosePreference = $verbosity
 }
