242e85dfe9c9f709db02c0c1bbbe445279197f87
CAndRyan <CAndRyan@users.noreply.github.com>
2017-08-24 22:00:59 -0400

    updated for newer tooling

--- CSharp/ScratchPad/Martini.cs
+++ CSharp/ScratchPad/Martini.cs
@@ -5,13 +5,30 @@ using System.Text;
 namespace ScratchPad {
 	public partial class Program {
 		public class Martini : IDriver {
+			public MartiniMethod Method;
+			private string _MethodName;
+			public string MethodName {
+				get {
+					return _MethodName;
+				}
+				private set {
+					_MethodName = value;
+				}
+			}
+			
+			public Martini(MartiniMethod method) {
+				Method = method;
+				MethodName = Enum.GetName(typeof(MartiniMethod), method);
+			}
+			public Martini() : this(MartiniMethod.Performant) { }
+
 			public void Main(string[] args) {
 				for (int i = 0; i < args.Length; i++) {
 					int val;
 					if (Int32.TryParse(args[i], out val)) {
-						Console.WriteLine("Martini (size={0}):", val.ToString());
-						if (!PrintGlass(val)) {
-							Console.Error.WriteLine("Input out of range: '{0}'", val.ToString());
+						Console.WriteLine("Martini (size: {0}, method: {1}):", val.ToString(), MethodName);
+						if (!Print(val)) {
+							Console.Error.WriteLine("Invalid input: '{0}'", val.ToString());
 						}
 					}
 					else {
@@ -20,7 +37,88 @@ namespace ScratchPad {
 				}
 			}
 
-			public static bool PrintGlass(int size) {
+			private bool Print(int size) {
+				switch (Method) {
+					case MartiniMethod.Performant:
+						throw new NotImplementedException("No method has yet been determined as 'most performant'");
+					case MartiniMethod.Slick:
+						return PrintGlassSlick(size);
+					case MartiniMethod.Modified:
+						return PrintGlassModifed(size);
+					case MartiniMethod.Original:
+						return PrintGlassOriginal(size);
+					case MartiniMethod.Basic:
+						return PrintGlassBasic(size);
+					default:
+						return false;
+				}
+			}
+
+			// public static bool PrintGlass(int size) {
+			// 	if (size < 1) { return false; }
+				
+			// 	StringBuilder sb = new StringBuilder();
+			// 	int maxWidth = size * 2 - 1;
+			// 	int mid = (maxWidth - 1) / 2;
+				
+			// 	for (int i = 0; i < maxWidth; i = i + 2) {
+			// 		Console.WriteLine(new String(' ', i / 2) + new String('0', maxWidth - i));
+			// 		sb.AppendLine(new String(' ', mid) + '|');
+			// 	}
+				
+			// 	sb.AppendLine(new String('=', maxWidth));
+			// 	Console.WriteLine(sb.ToString());
+
+			// 	return true;
+			// }
+
+			/// <summary>
+			/// Print a martini glass to the console --- 1 StringBuilder
+			/// </summary>
+			public static bool PrintGlassSlick(int size) {
+				if (size < 1) { return false; }
+				
+				StringBuilder sb = new StringBuilder();
+				string stemLine = String.Format("{0}{1}", new String(' ', size - 1), "|");
+
+				for (int i = size; i > 0; i--) {
+					sb.Insert(0, String.Format("{0}{1}{2}", new String(' ', i - 1), new String('0', 2 * (size - i) + 1), Environment.NewLine));
+					sb.AppendLine(stemLine);
+				}
+				
+				sb.AppendLine(new String('=', size * 2 - 1));
+				Console.Write(sb.ToString());
+
+				return true;
+			}
+
+			/// <summary>
+			/// Print a martini glass to the console --- 2 StringBuilders
+			/// </summary>
+			public static bool PrintGlassModifed(int size) {
+				if (size < 1) { return false; }
+				
+				StringBuilder sbTop = new StringBuilder();
+				StringBuilder sbBottom = new StringBuilder();
+				int maxWidth = size * 2 - 1;
+				int mid = (maxWidth - 1) / 2;
+				
+				for (int i = 0; i < maxWidth; i = i + 2) {
+					sbTop.AppendLine(new String(' ', i / 2) + new String('0', maxWidth - i));
+					sbBottom.AppendLine(new String(' ', mid) + '|');
+				}
+				sbBottom.AppendLine(new String('=', maxWidth));
+
+				Console.Write(sbTop.ToString());
+				Console.Write(sbBottom.ToString());
+
+				return true;
+			}
+
+			/// <summary>
+			/// Print a martini glass to the console --- 1 StringBuilder and direct-to-console
+			/// </summary>
+			public static bool PrintGlassOriginal(int size) {
 				if (size < 1) { return false; }
 				
 				StringBuilder sb = new StringBuilder();
@@ -33,10 +131,58 @@ namespace ScratchPad {
 				}
 				
 				sb.AppendLine(new String('=', maxWidth));
-				Console.WriteLine(sb.ToString());
+				Console.Write(sb.ToString());
 
 				return true;
 			}
+
+			/// <summary>
+			/// Print a martini glass to the console --- direct-to-console (2 loops)
+			/// </summary>
+			public static bool PrintGlassBasic(int size) {
+				if (size < 1) { return false; }
+				
+				int maxWidth = size * 2 - 1;
+				int mid = (maxWidth - 1) / 2;
+				
+				for (int i = 0; i < maxWidth; i = i + 2) {
+					Console.WriteLine(new String(' ', i / 2) + new String('0', maxWidth - i));
+				}
+				for (int i = 0; i < maxWidth; i = i + 2) {
+					Console.WriteLine(new String(' ', mid) + '|');
+				}
+				Console.WriteLine(new String('=', maxWidth));
+
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Most to least performant after some quick testing (size=10, times=100): 
+		/// Slick(1) avg=, Modified(2) avg=, Original(3) avg=, Basic(4) avg=
+		/// </summary>
+		public enum MartiniMethod {
+			/// <summary>
+			/// Whichever is the most performant of the actual methods
+			/// </summary>
+			Performant,
+			/// <summary>
+			/// Uses a single StringBuilder to build from the middle out within a single loop
+			/// </summary>
+			Slick,
+			/// <summary>
+			/// Uses 2 StringBuilders (top and bottom) within a single loop
+			/// </summary>
+			Modified,
+			/// <summary>
+			/// The original method, which uses a StringBuilder for the bottom and prints the top direct-to-console
+			/// all within a single loop
+			/// </summary>
+			Original,
+			/// <summary>
+			/// A naive approach that uses 2 loops (top and bottom) and prints direct-to-console
+			/// </summary>
+			Basic
 		}
 	}
 }
--- CSharp/ScratchPad/Program.cs
+++ CSharp/ScratchPad/Program.cs
@@ -1,24 +1,63 @@
 ﻿using System;
 using System.Linq;
+using System.Diagnostics;
 
 namespace ScratchPad {
     public partial class Program {
         public static void Main(string[] args) {
             if (args.Length > 0) {
                 IDriver driver = null;
+                Stopwatch watch = null;
+                int loopCount = 1;
+                long[] WatchTicks = new long[loopCount];
+
+                if (args.Length > 1 && args[args.Length - 1].StartsWith("--measure", StringComparison.OrdinalIgnoreCase)) {
+                    Int32.TryParse(args[args.Length - 1].ToLower().Replace("--measure", ""), out loopCount);
+                    watch = new Stopwatch();
+                    args = args.Take(args.Length - 1).ToArray();
+                }
 
                 switch (args[0].ToLower()) {
-                    case "--martini": 
-                        driver = new Martini();
+                    case "--martini":
+                        if (args.Length > 1 && args[1].StartsWith("-")) {
+                            MartiniMethod method;
+                            if (Enum.TryParse(args[1].Replace("-", ""), out method) && Enum.IsDefined(typeof(MartiniMethod), method)) {
+                                driver = new Martini(method);
+                                args = args.Skip(1).ToArray();
+                            }
+                            else {
+                                Console.Error.WriteLine("Invalid method value: '{0}'", args[1]);
+                                return;
+                            }
+                        }
+                        else {
+                            driver = new Martini();
+                        }
                         break;
                     default:
                         Console.Error.WriteLine("Invalid command: '{0}'", args[0]);
-                        break;
+                        return;
                 }
-
-                if (driver != null) {
-                    driver.Main(args.Skip(1).ToArray());
-                }
+                
+                do {
+                    watch?.Start();
+                    try {
+                        driver.Main(args.Skip(1).ToArray());
+                    }
+                    catch (Exception ex) {
+                        Console.Error.WriteLine("Error Encountered: {0}: {1}\n{2}", ex.GetType().FullName, ex.Message, ex.StackTrace);
+                        loopCount = 0;
+                    }
+                    finally {
+                        watch?.Stop();
+                        if (watch != null) {
+                            Console.WriteLine("Elapsed time (ticks): {0}", watch.ElapsedTicks);
+                            WatchTicks[loopCount] = watch.ElapsedTicks;
+                            watch.Reset();
+                        }
+                        loopCount--;
+                    }
+                } while (loopCount > 0)
             }
         }
 
