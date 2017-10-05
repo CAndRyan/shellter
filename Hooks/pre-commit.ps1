dcba0be943468a3a49d2acce4c530d3d1f5dfb4a
CAndRyan <CAndRyan@users.noreply.github.com>
2017-10-05 14:58:37 -0400

    updated test hooks

--- Hooks/pre-commit.ps1
+++ Hooks/pre-commit.ps1
@@ -1,3 +1,5 @@
 $dir = ("c:" + ("$($args[0])".Substring(2) -replace "/", "\"))
 
 New-Item -Path "$dir\..\..\TEST.txt" |Out-Null
+
+Exit 100
--- Hooks/pre-commit.sh
+++ Hooks/pre-commit.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 /c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -NoProfile -File $DIR"/pre-commit.ps1" $DIR
 
-rc = $?;
-if 
-	[[ $rc != 0 ]] 
-then 
+rc=$?
+if [ ! $rc -eq 0 ]; then
+	echo -e "${RED}Pre-Commit script failed with code: $rc${NC}"
 	exit $rc
 fi
