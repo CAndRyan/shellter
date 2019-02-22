16aac1fdc2eed553948a338f32d5944ba752d94c
CAndRyan <CAndRyan@users.noreply.github.com>
2019-02-22 05:51:49 -0500

    Add param block to Invoke-ThrowCommand function

--- Modules/Ventriloquism.psm1
+++ Modules/Ventriloquism.psm1
@@ -1,3 +1,14 @@
 Function Invoke-ThrowCommand {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ScriptBlock]
+        $Command,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ -PathType Container})]
+        [string]
+        $Location
+    )
+
     #
 }
