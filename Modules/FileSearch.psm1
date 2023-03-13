4f2c14ed0230f076954b98400e123189d465447c
CAndRyan <CAndRyan@users.noreply.github.com>
2023-03-13 15:27:17 -0400

    Cleanup file

--- Modules/FileSearch.psm1
+++ Modules/FileSearch.psm1
@@ -25,6 +25,7 @@ function Get-LineNumber {
 }
 
 function Search-ForTextAcrossFiles {
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)][string]$Path,
         [Parameter(Mandatory=$true)][string]$Text,  # TODO: support pattern input
@@ -36,8 +37,9 @@ function Search-ForTextAcrossFiles {
 
     $escapedText = $Text -replace '([\.\[\][(\)\\])','\$1'
     $pattern = "`n?(.*($escapedText).*)`n"
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
-    Get-ChildItem -Path $Path -Filter $FileFilter -File -Recurse:$(-not $RecurseDisabled) |
+    $results = Get-ChildItem -Path $Path -Filter $FileFilter -File -Recurse:$(-not $RecurseDisabled) |
         Select-Object -ExpandProperty FullName |
         ForEach-Object {
             $filePath = $_
@@ -45,24 +47,37 @@ function Search-ForTextAcrossFiles {
                 Select-String -Pattern $pattern -AllMatches:$(-not $FirstMatchOnly)
 
             if ($null -ne $matchInfo) {
-                $matches = $matchInfo.Matches |ForEach-Object {
+                $innerResults = $matchInfo.Matches |ForEach-Object {
                     $groups = $_.Groups
+
+                    if ($SkipLineNumber) {
+                        return New-Object PSCustomObject -Property @{
+                            Line = $groups[1].Value;
+                        }
+                    }
+                    
                     $startLineIndex = $groups[1].Index
                     $innerMatchStartIndex = $groups[2].Index - $startLineIndex
                     $innerMatchLength = $groups[2].Length
+                    $lineNumber = Get-LineNumber $filePath $startLineIndex
 
-                    New-Object PSCustomObject -Property @{
+                    return New-Object PSCustomObject -Property @{
                         Line = $groups[1].Value;
                         StartIndex = $innerMatchStartIndex;
                         EndIndex = $innerMatchStartIndex + $innerMatchLength - 1;
-                        LineNumber = $(-not $SkipLineNumber) ? $(Get-LineNumber $filePath $startLineIndex) : $null;
+                        LineNumber = $lineNumber;
                     }
                 }
 
                 New-Object PSCustomObject -Property @{
                     File = $filePath;
-                    Matches = $matches;
+                    Matches = $innerResults;
                 }
             }
         }
+
+    $stopwatch.Stop()
+    Write-Verbose "Search elapsed time > $($stopwatch.Elapsed.ToString())"
+
+    return $results
 }
